772d804ebaec4e4cec2fdc9d99276e78f3ec516c
yujin <sdyj178@gmail.com>
2023-02-03 09:05:22 +0900

    20230203-solved-H413: 규칙을 파악해서 해결함

---
+++ AlgoProblems/Softeer/src/지도_자동_구축/지도_자동_구축.java
@@ -0,0 +1,15 @@
+package 지도_자동_구축;
+
+import java.util.Scanner;
+
+public class 지도_자동_구축 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int ans = 2;
+        for (int i = 0; i < N; i++) {
+            ans = 2 * ans - 1;
+        }
+        System.out.println(ans * ans);
+    }
+}
