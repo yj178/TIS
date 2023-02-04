f129f8238a80aff7045d67f531e1909772ca60b9
yujin <sdyj178@gmail.com>
2023-02-04 20:42:03 +0900

    20230204-solved-B12015: 최장 증가 수열을 이용하여 문제 해결 #LIS #binary search arr에 숫자를 순서대로 입력받고, table에 각 최장 수열의 마지막 숫자를 기록한다.
    이때, binary search를 이용해서 log n 시간동안 현재 숫자가 추가 될 수 있는 최장 수열을 찾고 없다면 새로 기록, 있다면 기존의 최장 수열의 마지막 숫자보다 작은 경우 업데이트를 진행한다.
    
    가장 긴 길이를 기록하여 출력한다.

---
+++ AlgoProblems/Baekjoon/src/가장_긴_증가하는_부분_수열_2/가장_긴_증가하는_부분_수열2.java
@@ -0,0 +1,62 @@
+package 가장_긴_증가하는_부분_수열_2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 가장_긴_증가하는_부분_수열2 {
+    static int N;
+    static int[] arr;
+    static int[] table;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        init(br);
+
+        System.out.println(chklen());
+
+    }
+
+    static int chklen() {
+        int max = 0;
+        int ans = 0;
+        for (int n = 1; n <= N; n++) {
+            int idx = binarySearch(arr[n], ans);
+//            System.out.println("search : " + arr[n] +", "+ idx);
+            max = idx + 1;
+            if (table[idx] < arr[n]) {
+                table[max] = Math.min(table[max], arr[n]);
+                ans = Math.max(max, ans);
+            }
+//            System.out.println(Arrays.toString(table) + ", max : " + max + ", search : " + arr[n]);
+        }
+
+        return ans;
+    }
+
+    static int binarySearch(int target, int end) {
+        int s = 0;
+        int e = end;
+        int mid = 0;
+
+        while (s <= e) {
+            mid = (s + e) / 2;
+            if (table[mid] == target) return mid;
+            else if (table[mid] < target) s = mid + 1;
+            else e = mid - 1;
+        }
+        return e;
+    }
+
+    static void init(BufferedReader br) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        String[] tmp = br.readLine().split(" ");
+        arr = new int[N + 1];
+        table = new int[N + 1];
+        for (int n = 1; n <= N; n++) {
+            table[n] = Integer.MAX_VALUE;
+            arr[n] = Integer.parseInt(tmp[n - 1]);
+        }
+        table[0] = 0;
+    }
+}
