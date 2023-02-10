f3bb425153b62356aa7d021fe93bfa88367eba3e
yujin <sdyj178@gmail.com>
2023-02-10 16:17:19 +0900

    20230210-solved-H654-#14 통근버스 출발 순서 검증하기 통과
    누적합을 이용하여 N^3 -> N^2으로 연산량을 줄여야 한다.
    
    #prefix sum

---
+++ AlgoProblems/Softeer/src/Lv3/통근버스_출발_순서_검증하기/H654.java
@@ -0,0 +1,40 @@
+package Lv3.통근버스_출발_순서_검증하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class H654 {
+    static int[] arr;
+    static int[][] cnt;
+    static int N;
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        cnt = new int[N][N];
+        String[] tmp = br.readLine().split(" ");
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(tmp[i]);
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > arr[N - 1]) cnt[i][N - 1] = 1;
+            for (int j = N - 2; j > i; j--) {
+                if (arr[i] > arr[j]) cnt[i][j] = cnt[i][j + 1] + 1;
+                else cnt[i][j] = cnt[i][j + 1];
+            }
+        }
+
+
+        long ans = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N - 1; j++) {
+                if (arr[i] < arr[j]) ans += cnt[i][j];
+            }
+        }
+        System.out.println(ans);
+    }
+}
